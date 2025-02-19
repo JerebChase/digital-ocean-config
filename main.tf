9562d4732d1139ec15a96a824bec1c116b5159f2
Jeremy Brown <chase208@4JLT.com>
2025-02-18 23:21:46 -0500

    Trying to add a droplet

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 module "project" {
   source = "./modules/project"
 }
+
+module "droplet" {
+  source     = "./modules/droplet"
+  project_id = module.project.project_id
+}
---
+++ modules/droplet/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "tls" {}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "ED25519" 
+}
+
+resource "digitalocean_ssh_key" "terraform_ssh_key" {
+  name       = "terraform-ssh-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+resource "digitalocean_droplet" "droplet" {
+  name   = "test-droplet"
+  image  = "ubuntu-18-04-x64"
+  region = "nyc3"
+  size   = "s-1vcpu-2gb"
+  ssh_keys = [
+    digitalocean_ssh_key.terraform_ssh_key.id
+  ]
+}

beda3df9e035ac0f00515f608d126fe7c8bee560
Jeremy Brown <chase208@4JLT.com>
2025-02-28 12:06:41 -0500

    Trying to create crossplane resources with argo

--- modules/droplet/main.tf
+++ modules/droplet/main.tf
@@ -35,34 +35,6 @@ resource "digitalocean_droplet" "droplet" {
     private_key = tls_private_key.ssh_key.private_key_pem
     timeout = "2m"
   }
-  
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     # install k3s
-  #     "curl -sfL https://get.k3s.io | sh -",
-  #     "echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc",
-  #     "source ~/.bashrc",
-  #     # install argocd
-  #     "kubectl create namespace argocd",
-  #     "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml",
-  #     "kubectl patch svc argocd-server -n argocd -p '{\"spec\": {\"type\": \"LoadBalancer\"}}'",
-  #     # install UXP Crossplane
-  #     "kubectl create namespace crossplane",
-  #     "curl -sL https://cli.upbound.io | sh",
-  #     "sudo mv up /usr/local/bin/",
-  #     "up uxp install -n crossplane",
-  #     # install Helm
-  #     "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3",
-  #     "chmod 700 get_helm.sh",
-  #     "./get_helm.sh",
-  #     # install Port K8s Exporter
-  #     "helm repo add --force-update port-labs https://port-labs.github.io/helm-charts",
-  #     "echo 'PORT_CLIENT_ID=${var.port_client_id}' > /tmp/port_secrets.env",
-  #     "echo 'PORT_CLIENT_SECRET=${var.port_client_secret}' >> /tmp/port_secrets.env",
-  #     "source /tmp/port_secrets.env"
-  #     #"helm upgrade --install my-cluster port-labs/port-k8s-exporter --create-namespace --namespace port-k8s-exporter --set secret.secrets.portClientId=\"${var.port_client_id}\" --set secret.secrets.portClientSecret=\"${var.port_client_secret}\" --set portBaseUrl=\"https://api.getport.io\" --set stateKey=\"my-cluster\" --set integration.eventListener.type=\"POLLING\" --set \"extraEnv[0].name\"=\"CLUSTER_NAME\" --set \"extraEnv[0].value\"=\"my-cluster\" > /tmp/helm_install.log 2>&1"
-  #   ]
-  # }
 
   user_data = <<-EOF
     #!/bin/bash
@@ -105,6 +77,13 @@ resource "digitalocean_droplet" "droplet" {
       --set integration.eventListener.type="POLLING" \
       --set "extraEnv[0].name"="CLUSTER_NAME" \
       --set "extraEnv[0].value"="my-cluster"
+
+    # Install argocd app
+    argocd app create gitops-config \
+      --repo https://github.com/JerebChase/gitops-config.git \
+      --dest-server https://kubernetes.default.svc \
+      --dest-namespace argocd \
+      --sync-policy automated
 
     echo "Setup complete!"
   EOF

dac158dc1c2242977f13d25c39e89cf45e0f273c
Jeremy Brown <chase208@4JLT.com>
2025-03-09 07:24:48 -0400

    Updating the urls for the raw content files

--- modules/droplet/main.tf
+++ modules/droplet/main.tf
@@ -91,8 +91,8 @@ resource "digitalocean_droplet" "droplet" {
 
     # Install ArgoCD
     kubectl apply -k https://github.com/JerebChase/gitops-config.git//argocd/install
-    kubectl apply -f https://github.com/JerebChase/gitops-config.git//argocd/crossplane-bootstrap/crossplane-helm-secret.yml
-    kubectl apply -n argocd -f https://github.com/JerebChase/gitops-config.git//argocd/crossplane-bootstrap/crossplane.yml
+    kubectl apply -f https://raw.githubusercontent.com/JerebChase/gitops-config/main/argocd/crossplane-bootstrap/crossplane-helm-secret.yml
+    kubectl apply -n argocd -f https://raw.githubusercontent.com/JerebChase/gitops-config/main/argocd/crossplane-bootstrap/crossplane.yml
 
     echo "Setup complete!"
   EOF
